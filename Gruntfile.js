6f2ba21e931dc3cda1f8bdab8aac990c5574c88c
Michael Allan <michaelallan@optushome.com.au>
2013-11-04 17:24:36 +1100

    add some bower foundation tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,7 @@ module.exports = function (grunt) {
           require: 'zurb-foundation'
         }
       },
+
       dist: {
         options: {
           sassDir: 'sass',
@@ -26,6 +27,43 @@ module.exports = function (grunt) {
           require: 'zurb-foundation',
           environment: 'production'
         }
+      },
+
+      // bower set up for foundation with icons
+      // cf. https://gist.github.com/xseignard/5663623
+      foundation: {
+        options: {
+          sassDir: 'public/components/foundation/scss',
+          cssDir: 'public/stylesheets/foundation'
+        }
+      },
+
+      icons: {
+        options: {
+          sassDir: 'public/components/foundation-icons/foundation_icons_general/sass',
+          cssDir: 'public/stylesheets/foundation'
+        }
+      },
+
+      social: {
+        options: {
+          sassDir: 'public/components/foundation/foundation-icons/foundation_icons_social/sass',
+          cssDir: 'public/stylesheets/foundation'
+        }
+      }
+    },
+
+    copy: {
+      foundation: {
+        files: [{
+          expand: true,
+          flatten: true,
+          src: [
+            'public/components/foundation-icons/foundation_icons_general/fonts/*',
+            'public/components/foundation-icons/foundation_icons_social/fonts/*'
+          ],
+          dest: 'public/stylesheets/foundation/fonts'
+        }]
       }
     },
 
@@ -38,8 +76,13 @@ module.exports = function (grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
+
+  grunt.registerTask('foundation-dev', [
+    'compass:foundation', 'compass:icons', 'compass:social', 'copy:foundation'
+  ]);
 
   grunt.task.run('compass:dev');
 
